ba6adc527abf21ac493e2513f605864dd7409d92
Karl Henrik <KarlHenrik@users.noreply.github.com>
2020-10-23 09:54:32 +0200

    Mye som ikke funker!

---
+++ Project2/Code/gdOptimizers.py
@@ -0,0 +1,51 @@
+#The class GDOptimizers contains functionality for batch SGD and batch momentum SGD. It hopefully works for mimimizing functions with parameters
+#theta in any shape. If you want to expand the functionality to include AdaGrad or RMSProp which works for a theta with any shape you will need
+#to use a very different structure from what was used for SGD and momentumSGD. If the parameters theta are all in a 1D vector however it will be easy.
+import numpy as np
+
+class GDOptimizers:
+    def __init__(self, learning_rate, epochs, batch_size, optimizer = "SGD", alpha = 0):
+        self._learning_rate = learning_rate
+        self._epochs = epochs
+        self._batch_size = batch_size
+        if optimizer == "SGD":
+            self.optimizer = self.SGD
+        if optimizer == "momentum":
+            self.optimizer = self.momentumSGD
+            self._alpha = alpha
+
+    def SGD(self, X, z, theta, gradFunc, params):
+        n_inputs = len(z)
+        # del opp i minibatches
+        data_indices = np.arange(n_inputs)
+        iterations = n_inputs // self._batch_size
+        for i in range(self._epochs):
+            for j in range(iterations):
+                # pick datapoints with replacement
+                chosen_datapoints = np.random.choice(data_indices, size = self._batch_size, replace=False)
+                # minibatch training data
+                Xbatch = X[chosen_datapoints]
+                zbatch = z[chosen_datapoints]
+                # finding gradient
+                g = gradFunc(Xbatch, zbatch, theta, params)
+                theta = theta - self._learning_rate * g
+        return theta
+    
+    def momentumSGD(self, X, z, theta, gradFunc, params):
+        n_inputs = len(z)
+        # del opp i minibatches
+        data_indices = np.arange(n_inputs)
+        iterations = n_inputs // self._batch_size
+        v = theta * 0 # to get same shape as theta
+        for i in range(self._epochs):
+            for j in range(iterations):
+                # pick datapoints with replacement
+                chosen_datapoints = np.random.choice(data_indices, size = self._batch_size, replace=False)
+                # minibatch training data
+                Xbatch = X[chosen_datapoints]
+                zbatch = z[chosen_datapoints]
+                # finding gradient
+                g = gradFunc(Xbatch, zbatch, theta, params)
+                v = self._alpha * v + self._learning_rate * g
+                theta = theta - v
+        return theta
---
+++ Project2/Code/linearReg.py
@@ -0,0 +1,61 @@
+#The class LinearReg contains a design matrix and can be used for training a model with parameters beta using a gradient descent method with a regularization parameter (like ridge)
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+class LinearReg():
+    def __init__(self, order, lmda):
+        self._lmda = lmda
+        self._order = order
+        self._p = int((order + 1) * (order + 2) / 2)
+        self._beta = np.random.randn(self._p)
+        self._scaler = StandardScaler() #subtracts mean from each feature and divides by the standard deviation
+        self._isFit = False
+    
+    def designMatrix(self, x, y):
+        n = x.size
+        X = np.zeros((n, self._p))
+        feature = 0
+        for x_power in range(self._order + 1):
+            for y_power in range(self._order - x_power + 1):
+                X[:, feature] = x**x_power * y**y_power
+                feature += 1
+        return X
+    
+    def gradient(self, X, z, beta, params):
+        return -2 * X.T @ (z - X @ beta) + params["lmda"] * 2 * beta
+    
+    def fit(self, xy, z, sgd):
+        x = xy[:, 0]
+        y = xy[:, 1]
+        X = self.designMatrix(x, y)
+        
+        self._scaler.fit(X)
+        X = self._scaler.transform(X)
+        X[:, 0] = 1 # scaling removed the intercept terms
+        
+        params = {"lmda": self._lmda}
+        self._beta = sgd.optimizer(X, z, self._beta, self.gradient, params)
+        self._isFit = True
+        return X @ self._beta
+    
+    def predict(self, xy):
+        x = xy[:, 0]
+        y = xy[:, 1]
+        X_test = self.designMatrix(x, y)
+        if self._isFit:
+            X_test = self._scaler.transform(X_test)
+            X_test[:, 0] = 1 # scaling removed the intercept terms
+        return X_test @ self._beta
+    
+    def ridgeFit(self, xy, z): #in case we want to see what the "analytical" solution gives us
+        x = xy[:, 0]
+        y = xy[:, 1]
+        X = self.designMatrix(x, y)
+        
+        self._scaler.fit(X)
+        X = self._scaler.transform(X)
+        X[:, 0] = 1 # scaling removed the intercept terms
+        
+        self._beta = np.linalg.inv(X.T @ X + self._lmda * np.eye(self._p)) @ X.T @ z
+        self._isFit = True
+        return X @ self._beta
