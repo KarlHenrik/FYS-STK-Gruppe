c26cefedebe6e481a88a07bc027e580c1bf164e1
GullaTorvund <gulla.torv@gmail.com>
2020-12-14 21:47:45 +0100

    diffusionstuff, some changes in diffutils

--- Project3/Code/.ipynb_checkpoints/diffusionUtils-checkpoint.py
+++ Project3/Code/.ipynb_checkpoints/diffusionUtils-checkpoint.py
@@ -3,24 +3,24 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-def plot2D(x, t, U):
+def plot2D(x, t, U, funcname):
     dx = x[1] - x[0]
     
-    plt.title(f'dx = {dx}')
+    plt.title(f'{funcname} with dx = {dx}')
     for t_i in [0, int(len(t) * 0.1), int(len(t) * 0.3), int(len(t) * 0.4), int(len(t) * 0.8)]:
         plt.plot(x, U[:, t_i], label = f'Time =  {t[t_i]:0.3f}')
     plt.xlabel('x [m]')
     plt.ylabel('$u$ ')
     plt.legend()
         
-def plot3D(x, t, U):
+def plot3D(x, t, U, funcname):
     dx = x[1] - x[0]
     T, X = np.meshgrid(t, x)
 
     fig = plt.figure()
     ax = Axes3D(fig)
     #fig = plt.figure(projection='3d')
-    ax.set_title(f'dx = {dx}')
+    ax.set_title(f'{funcname} with dx = {dx}')
     ax.plot_surface(T, X, U, linewidth=0, antialiased=False, cmap=cm.viridis)
     ax.set_xlabel('Time $t$')
     ax.set_ylabel('Position $x$')
